dec4c8873221912d4398815e0bba1d6236043d90
Tou Xiong <tlxiong.prime@outlook.com>
2021-09-30 13:12:50 -0500

    presets for app

---
+++ src/components/Favorite/Favorite.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'; 
+import axios from 'axios';
+
+function Favorite(){
+    return (
+
+
+
+    );
+}
+
+export default Favorite;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import logger from 'redux-logger';
+import axios from 'axios';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { takeEvery, put } from 'redux-saga/effects' 
+import createSagaMiddleware from 'redux-saga';
+
+const categoryReducer = (state = [], action) => {
+
+}
+
+const favoritieReducer = (state = [], action) => {
+
+}
+
+// Sagas ------
+
+
+const sagaMiddleware = createSagaMiddleware();
+
+
+// createStore
+
+
+
+// sagaMiddleware.run('');
 
 ReactDOM.render(<App />, document.getElementById('root'));
