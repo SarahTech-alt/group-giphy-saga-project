e5481086281d2980eebcfd2c85a8ff231cfc716f
Tou Xiong <tlxiong.prime@outlook.com>
2021-09-30 14:24:05 -0500

    installed react-router react-router-dom, created routes to the two components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,14 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import Category from '../Category/Category.jsx';
 import Favorite from '../Favorite/Favorite.jsx';
 
-function App(props) {
+function App() {
 
   return (
     <div>
       <h1>Giphy Search!</h1>
       
       <Router>
-        
+
         <Route exact path='/' component={Category} />
 
         <Route exact path='/favorites' component={Favorite} />
